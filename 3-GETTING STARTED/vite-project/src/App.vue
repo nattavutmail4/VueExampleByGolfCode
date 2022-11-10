c7487e4eaf6668ececbcbbc90c613dacd66def57
AdminOffice <nattavutmail3@gmail.com>
2022-11-10 09:27:59 +0700

    แสดงวิธีการ init ให้ดูพร้อมการใช้งาน EsLine editor

---
+++ 3-GETTING STARTED/vite-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <table>
+      <thead>
+          <th>#</th>
+          <th>name</th>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
