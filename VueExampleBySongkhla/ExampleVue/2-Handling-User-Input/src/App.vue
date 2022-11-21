273e91e876e5636f6475e4de368bd2a77b4f6cb6
G <45158534+nattavutmail4@users.noreply.github.com>
2022-11-21 21:04:19 +0700

    Example vue

---
+++ VueExampleBySongkhla/ExampleVue/2-Handling-User-Input/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import {ref} from 'vue'
+  const message = ref('Hello World!')
+  const link = 'https://vuejs.org'
+  // function
+  const reverseMessage = async()=>{
+    message.value = await message.value.split('').reverse('').join('')
+  }
+  const notify = ()=>{
+    alert('navigation was prevented.')
+  }
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+  <button @click="reverseMessage">Reverse Message</button>
+  <!-- Can also be an in line expression statement -->
+  <button @click="message += '!'">Append "!"</button>
+  <a :href="link" @click.prevent="notify">
+    A linkwith e.preventDefault()
+  </a>
+</template>
+
+<style scoped>
+  button,a{
+     display: block;
+     margin-bottom: 1em;
+  }
+</style>
