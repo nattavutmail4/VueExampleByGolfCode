3e70119ad61f02199cae2392484e04c819ec9b15
G <45158534+nattavutmail4@users.noreply.github.com>
2022-11-21 19:46:30 +0700

    EP 8 การทำ emit slot ใน vue

---
+++ VueExampleBySongkhla/8-Components/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import {ref} from 'vue'
+  import ChildCompVue from './components/ChildComp.vue';
+  const greeting = ref('Hello from parent')
+  const childMsg = ref('No child msg yet')
+  const msg = ref('from parent')
+</script>
+
+<template>
+  <ChildCompVue :msg="greeting"></ChildCompVue><hr>
+  <h1>emit</h1>
+  <ChildCompVue @response="(msg_emit) => childMsg = msg_emit" />
+  <p>{{ childMsg }}</p>
+  <h1>slot</h1>
+  <ChildCompVue>Message: {{ msg }}</ChildCompVue>
+
+</template>
---
+++ VueExampleBySongkhla/8-Components/src/components/ChildComp.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import {ref} from 'vue'
+  defineProps({
+    msg:String,
+    msg_emit:String
+  })
+  const emit = defineEmits(['response'])
+  emit('response', 'hello from child')
+
+</script>
+<template>
+   <h1>การใช้งาน Components</h1>
+   <p>So far, we've only been working with a single component.
+    Real Vue applications are typically created with nested components.</p>
+    <hr>
+    <h2>{{ msg || 'No props passed yet' }}</h2>
+    <hr>
+    <h2>Child component</h2>
+    <slot>Fallback content</slot>
+</template>
