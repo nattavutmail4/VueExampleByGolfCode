950580c54ad8fb1e6e73185c0394ee5814035515
G <45158534+nattavutmail4@users.noreply.github.com>
2022-11-22 17:13:01 +0700

    ตัวอย่างที่ 3 - 4 ของ example รูปแบบ composition

---
+++ VueExampleBySongkhla/ExampleVue/3-AttributeBindings/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import {ref} from 'vue'
+
+  const message = ref('Hello World')
+  const isRed = ref(true)
+  const color = ref('green')
+
+  const toggleRed = ()=>{
+    isRed.value = ! isRed.value
+  }
+  const toggleColor = ()=>{
+    color.value = color.value === 'green' ? 'blue' : 'green'
+  }
+</script>
+
+<template>
+  <h1>การทำ Bindings</h1>
+  <p>
+    <span :title="message">
+       Hover your mouse over me for a few seconds to see my
+       dynamically bound title!
+    </span>
+  </p>
+  <p :class="{red:isRed}" @click="toggleColor">
+      This should be red... but click me toggle it.
+  </p>
+</template>
+<style>
+  .red{
+     color:red;
+  }
+</style>
---
+++ VueExampleBySongkhla/ExampleVue/4-Conditionals-and-loops/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { ref } from 'vue'
+  const show = ref(true)
+  const list = ref(['1','2','3'])
+</script>
+
+<template>
+  <button @click="show = !show">Toggle List</button>
+  <button @click="list.push(list.length+1)">Push number</button>
+  <button @click="list.pop()">Pop number</button>
+  <button @click="list.reverse()">Reverse List</button>
+  
+  <ul v-if="show && list.length">
+     <li v-for="item in list">{{item}}</li>
+  </ul>
+  <p v-else-if="list.length">List is not empty but hidden.</p>
+  <p v-else>List is empty</p>
+</template>
