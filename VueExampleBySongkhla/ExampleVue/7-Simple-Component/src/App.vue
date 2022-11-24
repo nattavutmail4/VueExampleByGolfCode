1062edb8db25b20d56ad6504a358582f9cbd5278
G <45158534+nattavutmail4@users.noreply.github.com>
2022-11-24 18:32:37 +0700

    ep7-

---
+++ VueExampleBySongkhla/ExampleVue/7-Simple-Component/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import {ref} from 'vue'
+  import TodoItem from './components/TodoItem.vue' // import component เข้ามา
+  let id =1;
+  //จำลองข้อมูลเป็น array in object
+  const groceryList = ref([
+    { id: id++, text: 'Vegetables' },
+    { id: id++, text: 'Cheese' },
+    { id: id++, text: 'Whatever else humans are supposed to eat' }
+  ])
+</script>
+<template>
+  <ol>
+     <!-- เรียกใช้งาน component todo
+        :todo เป็นการทำ props
+    -->
+     <TodoItem
+       v-for="item in groceryList"
+       :todo="item"
+       :key="item.id"
+     ></TodoItem>
+  </ol>
+</template>
