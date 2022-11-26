11c46b9b3af1de247972bf7190616609194d821d
G <45158534+nattavutmail4@users.noreply.github.com>
2022-11-26 17:52:40 +0700

    พื้นฐานครอสระบบจองห้องประชุมเบื้องต้น

---
+++ VueExampleBySongkhla/workshop-node-vue/ep1-example/src/components/Employees.vue
@@ -0,0 +1,40 @@
+<script setup>
+ import {ref} from 'vue'
+
+ let id = 1;
+ const data = [
+   {id:id++,name:'About',price:20000},
+   {id:id++,name:'bout',price:10000},
+   {id:id++,name:'out',price:30000},
+   {id:id++,name:'Dout',price:40000},
+   {id:id++,name:'Cbout',price:60000},
+   {id:id++,name:'Fout',price:70000},
+ ]
+</script>
+<template>
+  <section>
+    <div class="row">
+        <table width="600px">
+          <thead>
+              <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>price</th>
+              </tr>
+          </thead>
+          <tbody>
+             <tr v-if="data.length" v-for="emp in data" :key="emp.id">
+               <td>{{emp.id}}</td>
+               <td>{{emp.name}}</td>
+               <td>{{emp.price}}</td>
+            </tr>
+            <tr v-else>
+                <tr>
+                  <td colspan="3">no data</td>
+                </tr>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  </section>
+</template>
